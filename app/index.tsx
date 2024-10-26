5071053b8f2697636d0380b29e389d341a22c9a5
igoroliveira <igordeeoliveira@gmail.com>
2024-10-26 16:10:28 -0300

    new design

--- app/index.tsx
+++ app/index.tsx
@@ -1,125 +1,213 @@
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import {CameraView, CameraType, useCameraPermissions} from 'expo-camera';
 import {useEffect, useRef, useState} from 'react';
-import {Button, Pressable, StyleSheet, Text, View} from 'react-native';
+import {ActivityIndicator, Button, Pressable, StyleSheet, Text, View} from 'react-native';
 import axios from "axios";
 import * as Speech from 'expo-speech';
+import {HelloWave} from "@/components/HelloWave";
+import {LinearGradient} from "expo-linear-gradient";
+import {BlurView} from "expo-blur";
+import {Colors} from "@/constants/Colors";
+import {Image} from 'expo-image';
 
 export default function Index() {
-  const [processing, setProcessing] = useState<boolean>(false);
-  const [init, setInit] = useState<boolean>(false);
-  const [permission, requestPermission] = useCameraPermissions();
-  const cameraRef = useRef<CameraView>(null);
-  const initRef = useRef(init); // Cria uma ref para armazenar o valor atual de init
+    const [processing, setProcessing] = useState<boolean>(false);
+    const [init, setInit] = useState<boolean>(false);
+    const [permission, requestPermission] = useCameraPermissions();
+    const cameraRef = useRef<CameraView>(null);
+    const initRef = useRef(init); // Cria uma ref para armazenar o valor atual de init
 
-  useEffect(() => {
-    console.log(init)
-    initRef.current = init; // Atualiza a ref sempre que init muda
-    if (init) {
-      takeAndSpeak()
+    const controller = new AbortController();
+
+
+    useEffect(() => {
+        console.log(init)
+        initRef.current = init; // Atualiza a ref sempre que init muda
+        if (init) {
+            takeAndSpeak()
+        } else {
+            console.log('cancelando request')
+            controller.abort()
+        }
+
+    }, [init]);
+
+    if (!permission) {
+        // Camera permissions are still loading.
+        return <View/>;
     }
 
-  }, [init]);
+    if (!permission.granted) {
+        // Camera permissions are not granted yet.
+        return (
+            <LinearGradient
+                // Background Linear Gradient
+                colors={[Colors.argentinian_blue.DEFAULT, Colors.argentinian_blue["800"]]}
+                style={styles.background}
+            >
+                <View style={[styles.container]}>
+                    <Image
+                        style={styles.image}
+                        source={require("../assets/images/logoname.png")}
+                        contentFit="contain"
+                        transition={1000}
+                    />
+                    <BlurView intensity={60} style={styles.permission}>
+                        <HelloWave/>
+                        <Text style={[styles.text, {
+                            fontSize: 18,
+                            textAlign: 'center',
+                            color: Colors.cobalt_blue.DEFAULT
+                        }]}>Precisamos
+                            da permissão da camera para continuar</Text>
+                        <Button onPress={requestPermission} color={Colors.cobalt_blue.DEFAULT}
+                                title="Conceder permissão"/>
+                    </BlurView>
+                </View>
+            </LinearGradient>
+        );
+    }
 
-  if (!permission) {
-    // Camera permissions are still loading.
-    return <View />;
-  }
+    async function readImage(base64Image: string) {
+        axios.post('https://apivision.carlosp.dev/imagem',
+            {imagem_base64: base64Image},
+            {
+                signal: controller.signal,
+                headers: {
+                    Authorization: `Bearer ***REMOVED***`,
+                }
+            }).then(function (response) {
+            // handle success
+            const data: string = response?.data.data
+            console.log(data);
+            Speech.speak(data, {
+                language: 'pt', onDone: () => {
+                    console.log('terminou de falar', {init: init})
 
-  if (!permission.granted) {
-    // Camera permissions are not granted yet.
+                    if (initRef.current) {
+                        takeAndSpeak();
+                    }
+                }
+            });
+
+        }).catch(function (error) {
+            console.log(error);
+        }).finally(() => {
+            setProcessing(false);
+        })
+    }
+
+    async function takeAndSpeak() {
+        setProcessing(true);
+        const image = await takePhoto();
+        if (!image) {
+            console.log('image cant be found')
+            return;
+        }
+        await readImage(image);
+    }
+
+    async function takePhoto() {
+        console.log('take photo');
+        const image = await cameraRef.current?.takePictureAsync({base64: true, quality: 0.5});
+        return image?.base64
+    }
+
+    const btnBgColor = init ? 'red' : 'green';
+
+    //Camera view
     return (
         <View style={styles.container}>
-          <Text style={styles.text}>Precisamos da permissão da camera para continuar</Text>
-          <Button onPress={requestPermission} title="Conceder permissão" />
+            <CameraView style={styles.camera} animateShutter={false} ref={cameraRef}>
+                <View style={styles.overlayTop}>
+                    <Image
+                        style={styles.image}
+                        source={require("../assets/images/logoname.png")}
+                        contentFit="contain"
+                        transition={500}
+                    />
+                </View>
+                <BlurView intensity={100} style={styles.overlayBottom}>
+                    <Pressable style={[styles.button, {backgroundColor: btnBgColor}]} onPress={() => setInit(!init)}>
+                        <Text style={styles.text}>{init ? "Parar" : "Iniciar"}</Text>
+                    </Pressable>
+                    {processing && <View style={{flexDirection: 'row', gap: 8}}>
+                        <Text style={[styles.text, {color: Colors.cobalt_blue.DEFAULT}]}>Processando</Text>
+                        <ActivityIndicator color={Colors.cobalt_blue.DEFAULT}/>
+                    </View>}
+                </BlurView>
+
+            </CameraView>
         </View>
     );
-  }
-
-  async function readImage(base64Image: string) {
-    axios.post('https://apivision.carlosp.dev/imagem',
-    { imagem_base64: base64Image },
-    {
-      headers: {
-        Authorization: `Bearer `,
-      }
-    }).then(function (response) {
-      // handle success
-      const data: string = response?.data.data
-      console.log(data);
-      Speech.speak(data, {language: 'pt', onDone: () => {
-        console.log('terminou de falar', {init: init})
-
-        if (initRef.current) {
-          takeAndSpeak();
-        }
-      }});
-
-    }).catch(function (error) {
-      console.log(error);
-    }).finally(() => {
-      setProcessing(false);
-    })
-  }
-
-  async function takeAndSpeak() {
-    setProcessing(true);
-    const image = await takePhoto();
-    if (!image) {
-      console.log('image cant be found')
-      return;
-    }
-    await readImage(image);
-  }
-
-  async function takePhoto() {
-    console.log('take photo');
-    const image = await cameraRef.current?.takePictureAsync({base64: true, quality: 0.5});
-    return image?.base64
-  }
-
-  const btnBgColor = init ? 'red' : 'green';
-
-  return (
-      <View style={styles.container}>
-        <CameraView style={{ flex: 5}} animateShutter={false} ref={cameraRef}>
-        </CameraView>
-        <View style={styles.controller}>
-          <Pressable style={[styles.button, {backgroundColor: btnBgColor}]} onPress={() => setInit(!init)}>
-            <Text style={styles.text}>{init ? "Parar" : "Iniciar"}</Text>
-          </Pressable>
-          {processing && <Text style={styles.text}>Processando...</Text>}
-        </View>
-      </View>
-  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    flexDirection: 'column',
-    flex: 1
-  },
-  controller: {
-    flex: 1,
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    backgroundColor: "black"
+    image: {
+        flex: 1,
+        width: '45%'
+    },
+    permission: {
+        flex: 0,
+        gap: 40,
+        padding: 25,
+        margin: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
+        borderRadius: 25
+    },
+    overlay: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: "space-around",
+        backgroundColor: "transparent",
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 10,
+        paddingHorizontal: 26,
+        borderRadius: 4,
+        elevation: 1,
+        backgroundColor: 'green',
+    },
+    text: {
+        fontFamily: 'SpaceMono',
+        fontSize: 16,
+        color: 'white',
+    },
+    background: {
+        flex: 1
 
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 4,
-    elevation: 3,
-    backgroundColor: 'green',
-  },
-  text: {
-    fontFamily: 'SpaceMono',
-    fontSize: 16,
-    lineHeight: 21,
-    letterSpacing: 0.25,
-    color: 'white',
-  },
+    },
+    ///
+    container: {
+        flex: 1,
+    },
+    camera: {
+        flex: 1,
+    },
+    overlayTop: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        height: '15%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    overlayBottom: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: '15%', // Ajuste a altura conforme necessário
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10,
+        overflow: 'hidden',
+        backgroundColor: 'rgba(4, 67, 175, 0.4)',
+        borderTopLeftRadius: 35,
+        borderTopRightRadius: 35,
+    },
 });
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -7,20 +7,64 @@ const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
-};
+    'cobalt_blue': {
+        DEFAULT: '#0443AF',
+        100: '#010d23',
+        200: '#011b46',
+        300: '#022869',
+        400: '#03358c',
+        500: '#0443af',
+        600: '#055bf0',
+        700: '#3c82fb',
+        800: '#7dacfc',
+        900: '#bed5fe'
+    },
+    'black': {
+        DEFAULT: '#000101',
+        100: '#000000',
+        200: '#000000',
+        300: '#000000',
+        400: '#000000',
+        500: '#000101',
+        600: '#006666',
+        700: '#00cccc',
+        800: '#33ffff',
+        900: '#99ffff'
+    },
+    'argentinian_blue': {
+        DEFAULT: '#6EB9ED',
+        100: '#08273e',
+        200: '#0f4e7b',
+        300: '#1776b9',
+        400: '#309ae5',
+        500: '#6eb9ed',
+        600: '#8bc6f1',
+        700: '#a8d4f4',
+        800: '#c5e3f8',
+        900: '#e2f1fb'
+    },
+    'picton_blue': {
+        DEFAULT: '#50ACE4',
+        100: '#082435',
+        200: '#11486a',
+        300: '#196b9f',
+        400: '#218fd4',
+        500: '#50ace4',
+        600: '#72bbe9',
+        700: '#95ccee',
+        800: '#b8ddf4',
+        900: '#dceef9'
+    },
+    'palatinate_blue': {
+        DEFAULT: '#233BE5',
+        100: '#060a2f',
+        200: '#0b155f',
+        300: '#111f8e',
+        400: '#162abe',
+        500: '#233be5',
+        600: '#5062ea',
+        700: '#7c89f0',
+        800: '#a7b0f5',
+        900: '#d3d8fa'
+    }
+}
